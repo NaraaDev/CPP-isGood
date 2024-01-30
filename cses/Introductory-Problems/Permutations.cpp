12e5d5c8b21105d61e9ce4f53574b08b1bba12e4
NaraaDev <massive0304@gmail.com>
2024-01-31 07:11:34 +0800

    feat: Permutations on cses

---
+++ cses/Introductory-Problems/Permutations.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+
+#define ll long long
+#define f first
+#define s second
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n == 2 || n == 3)
+        cout << "NO SOLUTION";
+    else {
+        for(int i = 2; i <= n; i +=2)
+            cout << i << ' ';
+        for(int i = 1; i <= n; i+=2)
+            cout << i << ' ';
+    }
+}
+/*
+Never Give Up
+RILY <3
+*/
