7bd4d6275d9916e4a5e73e379dfaad2d0f81f129
NaraaDev <massive0304@gmail.com>
2024-03-05 02:33:33 +0800

    feat: cses sorting and searching problem ferris wheel

---
+++ cses/Sorting-and-Searching/Concert-Tickets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+#define ll long long
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
+/*
+Never Give Up
+RILY <3
+*/
--- cses/Sorting-and-Searching/Ferris-Wheel.cpp
+++ cses/Sorting-and-Searching/Ferris-Wheel.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include <climits>
 
 #define ll long long
 #define F first
@@ -14,33 +15,27 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,m;
-    cin >> n >> m;
-    int a[n];
 
+    int n, x, ans = 0;
+    cin >> n >> x;
+    int a[n];
     for(int i = 0; i < n; i++)
         cin >> a[i];
-    
-    int ans = 0;
 
     sort(a, a + n);
-    int currentSum = 0;
 
-    for(int i = n - 1; i >= 0; i--) {
-        currentSum += a[i];
-        if(currentSum > m) {
-            currentSum = a[i];
-            ans++;
-        }
-        if(currentSum == m) {
-            currentSum = 0;
-            ans++;
-        }
-    } 
-    if(currentSum != 0) {
+    int left = 0, right = n - 1;
+    
+    while(left <= right) {
+        if(a[left] + a[right] <= x)
+            left++;
+        right--;
         ans++;
     }
+
     cout << ans;
+
+
 }
 /*
 Never Give Up
